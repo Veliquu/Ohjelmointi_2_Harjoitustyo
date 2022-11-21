860aeacaaf1ea92ef1704a35eb741f050593c10b
Jesse <jesse.nikkanen@gmail.com>
2022-11-21 11:27:23 +0200

    Versio 0 sisältää selityksen mitä sovelluksen halutaan tekevän

---
+++ FrisbeegolfTulokset_Jesse/src/main/java/model/Tulokset.java
@@ -0,0 +1,63 @@
+package model;
+
+public class Tulokset {
+	
+	private int id;
+	private String date;
+	private String rata;
+	private double tuuli;
+	private int tulos;
+	public Tulokset(int id, String date, String rata, double tuuli, int tulos) {
+		super();
+		this.id = id;
+		this.date = date;
+		this.rata = rata;
+		this.tuuli = tuuli;
+		this.tulos = tulos;
+	}
+	public Tulokset() {
+		super();
+		this.id = 0;
+		this.date = null;
+		this.rata = null;
+		this.tuuli = 0.0;
+		this.tulos = 0;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public String getRata() {
+		return rata;
+	}
+	public double getTuuli() {
+		return tuuli;
+	}
+	public int getTulos() {
+		return tulos;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public void setRata(String rata) {
+		this.rata = rata;
+	}
+	public void setTuuli(double tuuli) {
+		this.tuuli = tuuli;
+	}
+	public void setTulos(int tulos) {
+		this.tulos = tulos;
+	}
+	@Override
+	public String toString() {
+		return "Tulokset: id = "+ id + ", päiväys =" + date + ", rata=" + rata + ", tuuli=" + tuuli +  "m/s" + ", tulos=" + tulos;    
+	}
+	
+	
+
+}
---
+++ FrisbeegolfTulokset_Jesse/src/main/java/program/KokeiluOhjelma.java
@@ -0,0 +1,11 @@
+package program;
+
+public class KokeiluOhjelma {
+
+	public static void main(String[] args) {
+		System.out.println("Ohjelman tarkoituksena on seurata frisbeegolf tuloksia eri radoilta. \nHaluan myös seurata tuulen nopeutta ja koska olen käynyt heittämässä.");
+		System.out.println("\nEli tarkoitus on käsitellä tuulen nopeutta, paikkaa, päivämäärää ja tulosta. \nMyös jos aika ja osaaminen riittää haluaisin saada piirrettyä graafin radan mukaan, missä näkyisi y-akselilla tulos ja x-akselilla päivä.");
+
+	}
+
+}
